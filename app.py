c992fe6e6c57800acc485c956bf8817aa47b6081
alepeteporico <tojandro@gmail.com>
2020-04-24 18:59:47 +0200

    actualizaciones

--- app.py
+++ app.py
@@ -11,4 +11,28 @@ def saludo(cadena):
     nombre = cadena
     return render_template("principal.html",nombre=nombre)
 
+@app.route('/potencia/<int:base>/<int:exponente>',methods=["GET","POST"])
+def calculadora(base,exponente):
+    base= base
+    exponente= exponente
+    if exponente==0:
+        resultado=1
+
+    elif exponente>0:
+        resultado=base**exponente
+
+    elif exponente<0:
+        resultado=1/base**exponente
+    return render_template("calculadora.html",base=base,exponente=exponente,resultado=resultado)
+
+@app.route('/cuenta/<palabra>/<letra>',methods=["GET","POST"])
+def cuenta_letras(palabra,letra):
+    palabra=palabra
+    letra=letra
+    if type(letra)==str:
+        num=palabra.count(letra)
+    else:
+        abort(404)
+    return render_template("letras.html",palabra=palabra,letra=letra,num=num)
+
 app.run(debug=True)
