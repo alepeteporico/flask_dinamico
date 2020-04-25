e8e3ebf53d17ef33de6ddc2561a3015b1f3902bd
alepeteporico <tojandro@gmail.com>
2020-04-25 12:41:39 +0200

    actualizaciones

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template
-from lxml import etree
-datos=etree.parse("libros.xml")
 app=Flask(__name__)
 
 
@@ -37,12 +35,5 @@ def cuenta_letras(palabra,letra):
     else:
         abort(404)
     return render_template("letras.html",palabra=palabra,letra=letra,num=num)
-
-@app.route('libro/<int:codigo>',methods=["GET","POST"])
-def libros(datos,codigo):
-    codigo=codigo
-    autor=(datos.xpath('//autor[codigo/text()="%i"]/text()'%codigo))
-    libro=(datos.xpath('//titulo[codigo/text()="%i"]/text()'%codigo))
-    return render_template("biblioteca.html",libro=libro,autor=autor)
 
 app.run(debug=True)
